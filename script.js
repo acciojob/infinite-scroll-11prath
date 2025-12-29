c2aa8798206319e2eb888ee247ac1b785a7955f8
11prath <90514562+11prath@users.noreply.github.com>
2025-12-29 15:01:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-//your code here!
+const list = document.getElementById("list");
 
+let count = 0;
+
+// function to add list items
+function addItems(number) {
+  for (let i = 0; i < number; i++) {
+    count++;
+    const li = document.createElement("li");
+    li.innerText = `Item ${count}`;
+    list.appendChild(li);
+  }
+}
+
+// add 10 items by default
+addItems(10);
+
+// infinite scroll
+list.addEventListener("scroll", () => {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItems(2);
+  }
+});
